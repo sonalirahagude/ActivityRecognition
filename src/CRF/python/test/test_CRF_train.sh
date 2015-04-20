46a60c1f6557d8ce675431462543b56cede27004
Sonali Rahagude <sonalirahagude@gmail.com>
2015-04-19 17:38:13 -0700

    cleaning up redundant files

--- src/CRF/python/test/test_CRF_train.sh
+++ src/CRF/python/test/test_CRF_train.sh
@@ -1,3 +1,3 @@
-#python ../sample_train.py ../../../preprocessing/R/test/crf_feature_disjoint crf.disjoint.model
-python ../sample_train.py ../../../preprocessing/R/test/crf_feature crf.model
+#python ../crf_train.py ../../../preprocessing/R/test/crf_feature_disjoint crf.disjoint.model
+python ../crf_train.py ../../../preprocessing/R/test/crf_feature crf.model
 ~/git/crfsuite-0.12/frontend/crfsuite dump crf.model > crf.model.friendly
